ebfdc6544db490e0fdb282270821122b239b4097
zonvista <lsrikar@gmail.com>
2024-07-30 23:14:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
